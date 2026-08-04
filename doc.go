4657068c3c71ec414c18bf065116b608347b144f
Rishabh Makhija <rishabh.makhija@cockroachlabs.com>
2026-08-04 15:16:05 +0530

    doc: fix grpc typo in package comment
    Trivial no-op to verify the central AI review path triggers automatically
    on a drpc PR (webhook -> eventgateway -> metric-ingestor dispatch). Do not
    merge unless someone wants the typo fix on its own.

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 // Copyright (C) 2019 Storj Labs, Inc.
 // See LICENSE for copying information.
 
-// Package drpc is a light replacement for gprc.
+// Package drpc is a light replacement for grpc.
 package drpc
